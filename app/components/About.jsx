b323ec8e111d6d79ec42c1e7444978b45e25b733
Andrew Harris <blakebharris@gmail.com>
2016-05-26 19:56:53 -0500

    styled about page and linked to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,9 +5,25 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3 className='text-center'>About</h3>
-      <p className='text-center'>This is my first attempt at a react app, and first attempt to be launched to heroku.</p>
+      <h1 className='text-center'>About</h1>
+      <p>
+        This is my first attempt at a react app, and first attempt to be launched
+      </p>
+      <p>
+        Here are some of the tools used:
+      </p>
+        <ul>
+          <li>
+            <a href='https://facebook.github.io/react'>React</a> - Javascript Framework
+          </li>
+          <li>
+            <a href='https://openweathermap.org'>Open Weather Map</a> - Weather API used.
+           </li>
+           <li>
+             <a href='http://github.com/bharris62'>My Github</a>  - Check source code here.
+           </li>
+        </ul>
     </div>
-  );
-}
+  )
+};
 module.exports = About;
