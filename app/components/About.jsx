301b7c5734396bec2d3c89f50ed2f2ab3a4639e3
Andrew Harris <blakebharris@gmail.com>
2016-06-01 20:30:08 -0500

    added style sheet

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h1 className='text-center'>About</h1>
+      <h1 className='text-center page-title'>About</h1>
       <p>
         This is my first attempt at a react app, and first attempt to be launched
       </p>
